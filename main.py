0caa2de50a8b738151900b9b1359fc525cbc8588
Jules-Arthus "JA" Klein <jules.arthus.klein@gmail.com>
2024-10-07 21:30:46 +0200

    renamed files to match usage

--- main.py
+++ main.py
@@ -1,27 +1,20 @@
-def gameNotEnded(board):
-    for i in board:
-        for j in i:
-            if j == 1:
-                return True
-    return False
+from backend import *
 
-def attack(board, x, y):
-    (x,y) = (y,x)
-    hit = False
-    for i in range(len(board)):
-        print()
-        print("Ligne " + str(i) + ":", end =" ")
-        for j in range(len(board[i])-1):
-            if(i == x and j == y):
-                if(board[i][j] == 1):
-                    print("\033[32m" + "X", end = " ")
-                    board[i][j] = 0
-                    hit = True
-                else:
-                    print("\033[31m" + str(board[x][y]), end = " ")
-                print("\033[0m", end = "")
-            else:
-                print(board[i][j], end=" ")     
-    print()
-    print("\033[32m" + "Touché !" + "\033[0m" if hit else "\033[31m" + "Raté !" + "\033[0m")
-    print()
+board = [
+[0,0,1,0,0,0,0,0,0,0],
+[0,0,0,1,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,1,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+]
+
+def play():
+    x = int(input("Choissisez une coordonnée X : "))
+    y = int(input("Choissisez une coordonnée Y : "))
+    
+    attack(board, x, y)
+    
+while(gameNotEnded(board)):
+    play()
+
+print("Partie terminée !")
