f7d35c9b8c551b69982dd9b4492db9097365a9f1
Jules-Arthus "JA" Klein <jules.arthus.klein@gmail.com>
2024-10-10 19:30:54 +0200

    oui

--- main.py
+++ main.py
@@ -17,7 +17,6 @@ printBoard(board)
 def play():
     x = int(input(f"Choissisez une coordonnée X comprise entre 0 et {tailleX} : "))
     y = int(input(f"Choissisez une coordonnée Y comprise entre 0 et {tailleY} : "))
-    
     attack(board, x, y)
     
 while(gameNotEnded(board)):
