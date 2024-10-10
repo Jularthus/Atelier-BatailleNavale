4b90681bae36b246d11009a01de4ef9bc4009527
Jules-Arthus "JA" Klein <jules.arthus.klein@gmail.com>
2024-10-10 19:57:51 +0200

    made ui better

--- backend.py
+++ backend.py
@@ -1,33 +1,36 @@
-def gameNotEnded(board):
+def jeuEnCours(board):
     for i in board:
         for j in i:
             if j == 1:
                 return True
     return False
 
-def getSizeX(board):
+def longueur(board):
     return len(board[0])-1
 
-def getSizeY(board):
+def largeur(board):
     return len(board)-1
 
-def printBoard(board):
+def afficherTableau(board):
+    print("\033[33m" + "  X =  0 1 2 3 4 5 6 7 8 9", end="")
+    print("\033[0m", end="")
     for i in range(len(board)):
         print()
-        print("Ligne " + str(i) + ":", end =" ")
+        print("\033[33m" +"Y = " + str(i) + ":", end ="\033[0m ")
         for j in range(len(board[i])):
-            print(str(board[i][j]), end = " ")
+            print("🛥️" if board[i][j] else "\033[34m~", end="\033[0m ")
     print()
 
-def attack(board, x, y):
+def attaquer(board, x, y):
     (x,y) = (y,x)
     if(x < 0 or x >= len(board) or y < 0 or y >= len(board[0])):
         print("\033[31m" + "Coordonnées invalides !" + "\033[0m")
         return
     hit = False
+    print("\033[33m" + "  X =  0 1 2 3 4 5 6 7 8 9", end="")
     for i in range(len(board)):
         print()
-        print("Ligne " + str(i) + ":", end =" ")
+        print("\033[33m" +"Y = " + str(i) + ":", end ="\033[0m ")
         for j in range(len(board[i])):
             if(i == x and j == y):
                 if(board[i][j] == 1):
@@ -38,7 +41,7 @@ def attack(board, x, y):
                     print("\033[31m" + str(board[x][y]), end = " ")
                 print("\033[0m", end = "")
             else:
-                print(board[i][j], end=" ")     
+                print("🛥️" if board[i][j] else "\033[34m~", end="\033[0m ")     
     print()
     print("\033[32m" + "Touché !" + "\033[0m" if hit else "\033[31m" + "Raté !" + "\033[0m")
     print()
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from backend import *
 
-board = [
+# On initialise le tableau de jeu, 0 est vide, 1 est un bateau
+tableau = [
 #0,1,2,3,4,5,6,7,8,9
 [0,0,1,0,0,0,0,0,0,0],
 [0,0,0,1,0,0,0,0,0,0],
@@ -9,17 +10,22 @@ board = [
 [0,0,0,0,0,0,0,0,0,0],
 ]
 
-tailleX = getSizeX(board)
-tailleY = getSizeY(board)
+# On récupère la taille du tableau de jeu
+longueur = longueur(tableau)
+largeur = largeur(tableau)
 
-printBoard(board)
+# On l'affiche une première fois pour commencer le jeu
+afficherTableau(tableau)
 
+# On demande au joueur de choisir des coordonnées, puis on attaque la case correspondante
 def play():
-    x = int(input(f"Choissisez une coordonnée X comprise entre 0 et {tailleX} : "))
-    y = int(input(f"Choissisez une coordonnée Y comprise entre 0 et {tailleY} : "))
-    attack(board, x, y)
+    y = int(input(f"Choissisez une coordonnée Y comprise entre 0 et {largeur} : "))
+    x = int(input(f"Choissisez une coordonnée X comprise entre 0 et {longueur} : "))
+    attaquer(tableau, x, y)
     
-while(gameNotEnded(board)):
+# On fait une boucle pour continuer à jouer tant que la partie n'est pas terminée
+while(jeuEnCours(tableau)):
     play()
 
+# Une fois la boucle terminée, le jeu est fini, on affiche un message de fin
 print("Partie terminée !")
